0170c9023fc24606d773a61060651336a500580e
TarikKaanKoc <tarikkaan1koc@gmail.com>
2023-04-03 03:24:44 +0300

    Update normalizer tests to include edge cases

--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -1,58 +1,62 @@
-from unittest import TestCase
+import unittest
 
 from mintlemon import Normalizer
 
 
-class TestNormalizer(TestCase):
-    """
-    TestNormalizer is a subclass of unittest.TestCase, and it contains methods for testing the functionality of the Normalizer class.
+class TestNormalizer(unittest.TestCase):
+    """Tests for the Normalizer class methods"""
 
-    The test methods in this class check the output of Normalizer class' methods against expected results.
-
-    Methods
-    -------
-    test_lower_case()
-        Test the lower_case method of Normalizer class by passing a sample text and comparing its output with the expected output.
-    test_remove_punctuations()
-        Test the remove_punctuations method of Normalizer class by passing a sample text and comparing its output with the expected output.
-    test_remove_accent_marks()
-        Test the remove_accent_marks method of Normalizer class by passing a sample text and comparing its output with the expected output.
-    test_convert_text_numbers()
-        Test the convert_text_numbers method of Normalizer class by passing a sample text and comparing its output with the expected output.
-    """
+    def setUp(self):
+        self.normalizer = Normalizer()
+        self.text_low = "Ex: İIĞÜÖŞÇ"
+        self.text_with_numbers = "2,5 kilgoram şeker ve 2 adet ekmek alabilir miyim?"
+        self.text_with_punctuations = "!,,,Merhaba? Dünya!,,,"
+        self.text_deasc = "O sirada bahcede cicekleri kokluyorduk. Hersey bahcivanin islik calmasiyla yasandi..."
+        self.text_accent_marks = "merhâbâ"
+        self.text_norm_turkish_chars = "Gazi Üniversitesi'ne hoş geldiniz."
+        self.text_with_mixed_numbers = "Bu cümle 12.34 ile başlıyor ve 56 ile bitiyor. 2,5 +3,5 -3,4 ile ilgili bir şeyler söyleyebiliriz."
 
     def test_lower_case(self):
-        text = "Ex: İIĞÜÖŞÇ"
-        self.assertEqual(Normalizer.lower_case(text), "ex: iığüöşç")
+        """Test the lower_case() method"""
+        self.assertEqual(self.normalizer.lower_case(self.text_low), "ex: iığüöşç")
 
     def test_remove_punctuations(self):
-        text = "#Merhaba, Dünya!"
-        self.assertEqual(Normalizer.remove_punctuations(text), "Merhaba Dünya")
+        """Test the remove_punctuations() method"""
+        self.assertEqual(
+            self.normalizer.remove_punctuations(self.text_with_punctuations),
+            "Merhaba Dünya",
+        )
 
     def test_remove_accent_marks(self):
-        text = "merhâbâ"
-        self.assertEqual(Normalizer.remove_accent_marks(text), "merhaba")
+        """Test the remove_accent_marks() method"""
+        self.assertEqual(
+            self.normalizer.remove_accent_marks(self.text_accent_marks), "merhaba"
+        )
 
     def test_convert_text_numbers(self):
-        text = "2,5 kilgoram şeker ve 2 adet ekmek alabilir miyim?"
+        """Test the convert_text_numbers() method"""
         self.assertEqual(
-            Normalizer.convert_text_numbers(text), "iki virgül beş kilgoram şeker ve iki adet ekmek alabilir miyim?"
+            self.normalizer.convert_text_numbers(self.text_with_numbers),
+            "iki virgül beş kilgoram şeker ve iki adet ekmek alabilir miyim?",
         )
 
     def test_deasciify(self):
-        deasciifier = Normalizer.Deasciifier(
-            "O sirada bahcede cıcekleri kokluyorduk. Hersey bahcıvanın islik calmasiyla yasandi..."
+        """Test the deasciify() method"""
+        self.assertEqual(
+            self.normalizer.deasciify(self.text_deasc),
+            "O sırada bahçede çiçekleri kokluyorduk. Herşey bahçıvanın ıslık çalmasıyla yaşandı...",
         )
 
-        result = deasciifier.deasciify()
-        expected_result = "O sırada bahçede çiçekleri kokluyorduk. Herşey bahçıvanın ıslık çalmasıyla yaşandı..."
-        self.assertEqual(result, expected_result)
-
-    def test_normalization(self):
+    def test_normalize_turkish_chars(self):
+        """Test the normalize_turkish_chars() method"""
         self.assertEqual(
-            Normalizer.normalize_turkish_chars("Bir gün İstanbul'da çay içtik."), "Bir gun Istanbul'da cay ictik."
-        )
-        self.assertEqual(
-            Normalizer.normalize_turkish_chars("Gazi Üniversitesi'ne hoş geldiniz."),
+            self.normalizer.normalize_turkish_chars(self.text_norm_turkish_chars),
             "Gazi Universitesi'ne hos geldiniz.",
+        )
+
+    def test_remove_numbers(self):
+        """Test the remove_numbers() method"""
+        self.assertEqual(
+            self.normalizer.remove_numbers(self.text_with_mixed_numbers),
+            "Bu cümle ile başlıyor ve ile bitiyor. ile ilgili bir şeyler söyleyebiliriz.",
         )
