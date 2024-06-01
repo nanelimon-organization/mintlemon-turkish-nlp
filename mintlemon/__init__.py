6a5d218b3e7b16df315866c82fd2a52846256425
TarikKaanKoc <tarikkaan1koc@gmail.com>
2024-06-01 19:54:16 +0300

    Bumped to stable version

--- mintlemon/__init__.py
+++ mintlemon/__init__.py
@@ -1,6 +1,5 @@
 from .sentence_splitter import SentenceSplitter
 from .normalizer import Normalizer
 from .normalizer import TextRootDTMVectorizer
-from .turkish_spellcheck import TurkishSpellChecker
 
 __all__ = ["SentenceSplitter", "Normalizer", "TextRootDTMVectorizer"]
